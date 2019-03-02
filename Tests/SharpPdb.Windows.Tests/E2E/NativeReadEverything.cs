593a6c3c9607819c85e9fef0ffcc5e3cd9d2c3e7
southpolenator <southpolenator@gmail.com>
2019-03-02 21:17:05 +0100

    Reading TPI stream in native tests.

--- Tests/SharpPdb.Windows.Tests/E2E/NativeReadEverything.cs
+++ Tests/SharpPdb.Windows.Tests/E2E/NativeReadEverything.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using SharpPdb.Windows.TPI;
+using System.Linq;
 using Xunit;
 
 namespace SharpPdb.Windows.Tests.E2E
@@ -12,12 +13,15 @@ namespace SharpPdb.Windows.Tests.E2E
             {
                 Assert.NotNull(pdb.PdbSymbolStream);
                 ReadSymbolStream(pdb.PdbSymbolStream);
+                Assert.NotNull(pdb.TpiStream);
+                ReadTpiStream(pdb.TpiStream);
             }
         }
 
         private void ReadSymbolStream(SymbolStream symbolStream)
         {
             var references = symbolStream.References;
+            Assert.NotNull(references);
             for (int i = 0; i < references.Count; i++)
             {
                 Assert.NotNull(symbolStream[i]);
@@ -26,6 +30,27 @@ namespace SharpPdb.Windows.Tests.E2E
             var kinds = references.Select(r => r.Kind).Distinct().ToArray();
             foreach (var kind in kinds)
                 Assert.NotEmpty(symbolStream[kind]);
+        }
+
+        private void ReadTpiStream(TpiStream tpiStream)
+        {
+            Assert.NotNull(tpiStream.HashValues);
+            if (tpiStream.HashSubstream != null && tpiStream.Header.HashAdjustersBuffer.Length > 0)
+                Assert.NotNull(tpiStream.HashAdjusters);
+            else
+                Assert.Null(tpiStream.HashAdjusters);
+            var references = tpiStream.References;
+            Assert.NotNull(references);
+            Assert.Equal(references.Count, tpiStream.TypeRecordCount);
+            for (int i = 0; i < references.Count; i++)
+                Assert.NotNull(tpiStream[TypeIndex.FromArrayIndex(i)]);
+            var kinds = references.Select(r => r.Kind).Distinct().ToArray();
+            foreach (var kind in kinds)
+            {
+                Assert.NotEmpty(tpiStream.GetIndexes(kind));
+                Assert.NotEmpty(tpiStream[kind]);
+            }
+            Assert.NotEmpty(tpiStream.TypeIndexOffsets);
         }
     }
 }
