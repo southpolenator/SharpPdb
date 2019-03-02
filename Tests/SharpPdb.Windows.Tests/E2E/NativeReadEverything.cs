e8d550de6ee92f8713213fed3b467c0ea9195bad
southpolenator <southpolenator@gmail.com>
2019-03-02 21:04:53 +0100

    Starting native read everything tests.

---
+++ Tests/SharpPdb.Windows.Tests/E2E/NativeReadEverything.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace SharpPdb.Windows.Tests.E2E
+{
+    public class NativeReadEverything : TestBase
+    {
+        [Fact]
+        public void Test1()
+        {
+            using (PdbFile pdb = OpenPdb(1))
+            {
+                Assert.NotNull(pdb.PdbSymbolStream);
+                ReadSymbolStream(pdb.PdbSymbolStream);
+            }
+        }
+
+        private void ReadSymbolStream(SymbolStream symbolStream)
+        {
+            var references = symbolStream.References;
+            for (int i = 0; i < references.Count; i++)
+            {
+                Assert.NotNull(symbolStream[i]);
+                Assert.Equal(symbolStream[i], symbolStream.GetSymbolRecordByOffset(references[i].DataOffset - RecordPrefix.Size));
+            }
+            var kinds = references.Select(r => r.Kind).Distinct().ToArray();
+            foreach (var kind in kinds)
+                Assert.NotEmpty(symbolStream[kind]);
+        }
+    }
+}
